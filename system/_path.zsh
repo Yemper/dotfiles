6bfadc19341ee4c44715247630e8c14a2c290e36
KensoDev <avi@kensodev.com>
2015-01-31 05:07:34 -0800

    RBENV shims come first, adding aliases

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,3 +1,3 @@
-export PATH="/usr/local/heroku/bin:$HOME/.rbenv/bin:$PATH"
+export PATH="$HOME/.rbenv/shims:/usr/local/heroku/bin:$HOME/.rbenv/bin:$PATH"
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,8 @@
 alias reload!='. ~/.zshrc'
+
+alias bi='bundle install'
+alias c='clear'
+
+alias tmux="TERM=screen-256color-bce tmux"
+
+alias migrate="bundle exec rake db:migrate && bundle exec rake db:test:clone"
