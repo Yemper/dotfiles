f1aacb549ddbe06841c753ef85a36bd25d51c156
KensoDev <avi@kensodev.com>
2017-03-21 10:50:27 -0700

    adding virtualenv support and some more tmux changes

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -2,6 +2,7 @@
 #   Made possible through contributions from generous benefactors like
 #   `brew install coreutils`
 if $(gls &>/dev/null)
+
 then
   alias ls="gls -F --color"
   alias l="gls -lAh --color"
