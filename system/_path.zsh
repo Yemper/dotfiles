059ff41ebdfe14229b91400857725e5fb21c68f5
KensoDev <avi@kensodev.com>
2017-04-10 12:49:04 -0700

    adding a condition to the bootstrap script

--- system/_path.zsh
+++ system/_path.zsh
@@ -2,5 +2,5 @@ export PATH="$HOME/.rbenv/shims:/usr/local/heroku/bin:$HOME/.rbenv/bin:$PATH"
 export PATH="/usr/local/bin:./bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export PATH="$HOME/Android:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-export ANDROID_HOME="$HOME/Android"
 export CDPATH=.:~:~/Code:~/Code/globality:~/Code/go/src/
+
